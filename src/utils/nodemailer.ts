d0a6fdcc911f8ef58cf96b6d48b54445b8e47cb1
Azizbek-h <texnoposnode@gmail.com>
2023-02-08 22:33:06 +0500

    nodemailer update

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -11,7 +11,7 @@ let transporter = nodemailer.createTransport({
   logger: true,
   debug: true,
   ignoreTLS: true,
-  auth: {
+  auth: { 
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
--- test.js
+++ test.js
@@ -3,38 +3,38 @@ require('dotenv').config();
 
 console.log(process.env.SMTP_USER)
 let transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "arzustartup@gmail.com",
+  service: "mail", 
+  host: "mail.kvartirabar.uz",
   port: 25,
   secure: false,
   logger: true,
   debug: true,
   ignoreTLS: true,
   auth: {
-    user: "arzustartup@gmail.com",
-    pass: "gccrcpdlljwndebo",
+    user: "arzu@kvartirabar.uz",
+    pass: "!7WQGyn5!Z4M",
   },
 });
 
 
 
-transporter.sendMail(
-    {
-    from: "arzustartup@gmail.com",
-    to: "azizbekcoderuz@gmail.com", // to user
-    subject: "[Arzu] Verify code",
-    text: "",
-    html: `<b>Код подтверждения:</b> <code>1212123</code>. <b>Никому</b> не давайте код, даже если его требуют от имени <b>Arzu!</b>
-    <br><br>Этот код используется для входа в Ваш аккаунт в <b>Arzu</b>. Он никогда не нужен для чего-либо еще. 
-    <br><br>Если Вы не запрашивали код для входа, проигнорируйте это сообщение.`,
-    },
-    (err, info) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(info)
-    }
-); 
+    transporter.sendMail(
+      {
+        from: "arzu@kvartirabar.uz",
+        to: "azizbekcoderuz@gmail.com", // to user
+        subject: "[Arzu] Verify code",
+        text: "",
+        html: `<b>Код подтверждения:</b> <code>1212123</code>. <b>Никому</b> не давайте код, даже если его требуют от имени <b>Arzu!</b>
+        <br><br>Этот код используется для входа в Ваш аккаунт в <b>Arzu</b>. Он никогда не нужен для чего-либо еще. 
+        <br><br>Если Вы не запрашивали код для входа, проигнорируйте это сообщение.`,
+      },
+      (err, info) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(info)
+      }
+    ); 
 
 
 
