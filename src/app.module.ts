e162d2334fd67b64ff5018cb6a421feb4a8d46c0
Azizbek-h <texnoposnode@gmail.com>
2023-02-08 16:45:07 +0500

    compress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
-import type { RedisClientOptions } from 'redis';
 import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'; 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmConfig } from './config/typeorm.config';
-import { RedisModule } from './redis/redis.module';
 import { UserModule } from './user/user.module';
-const redisStore = require("cache-manager-redis-store");
 
 @Module({
   imports: [
