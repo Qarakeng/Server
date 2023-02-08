9f2df98bc5fe5120c7ad5f7493b6ff1319a1d5d6
Azizbek-h <texnoposnode@gmail.com>
2023-02-08 23:08:47 +0500

    add update profile

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -2,15 +2,19 @@ import { IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-vali
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateProfileDto {
+    @IsOptional()
     @IsString()
     username: string;
 
+    @IsOptional()
     @IsString()
     first_name: string;
 
+    @IsOptional()
     @IsString()
     last_name: string;
      
+    @IsOptional()
     @IsString()
     @MinLength(0)
     @MaxLength(52)
