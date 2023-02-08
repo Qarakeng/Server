4b7d5f56aadb61a9ce4c84e1f5e02fa2c1a3461c
Azizbek-h <texnoposnode@gmail.com>
2023-02-08 21:09:20 +0500

    add prefix for route

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('/v1/api')
   
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => console.log(`🚀Listen on port ${PORT} 🚀`))
