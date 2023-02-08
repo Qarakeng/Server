a31aa80f7aa4dba916ba462d05785b3e76db3d80
Azizbek-h <texnoposnode@gmail.com>
2023-02-08 22:19:51 +0500

    nodemailer update

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -5,6 +5,12 @@ dotenv.config();
 console.log(process.env.SMTP_USER)
 let transporter = nodemailer.createTransport({
   service: process.env.SMTP_SERVICE,
+  host: process.env.SMTP_HOST,
+  port: 25,
+  secure: false,
+  logger: true,
+  debug: true,
+  ignoreTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
--- test.js
+++ test.js
@@ -3,35 +3,38 @@ require('dotenv').config();
 
 console.log(process.env.SMTP_USER)
 let transporter = nodemailer.createTransport({
-  service: process.env.SMTP_SERVICE,
+  service: "gmail",
+  host: "arzustartup@gmail.com",
+  port: 25,
+  secure: false,
+  logger: true,
+  debug: true,
+  ignoreTLS: true,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    user: "arzustartup@gmail.com",
+    pass: "gccrcpdlljwndebo",
   },
 });
 
-let SendMailer = async (to, randomCode) => {
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(
-      {
-        from: process.env.SMTP_USER,
-        to, // to user
-        subject: "[Arzu] Verify code",
-        text: "",
-        html: `<b>Код подтверждения:</b> <code>${randomCode}</code>. <b>Никому</b> не давайте код, даже если его требуют от имени <b>Arzu!</b>
-        <br><br>Этот код используется для входа в Ваш аккаунт в <b>Arzu</b>. Он никогда не нужен для чего-либо еще. 
-        <br><br>Если Вы не запрашивали код для входа, проигнорируйте это сообщение.`,
-      },
-      (err, info) => {
-        if (err) {
-            console.log(err)
-          reject(err)
-        }
-        console.log(info)
-        resolve(info)
-      }
-    ); 
-  })
-}; 
 
-SendMailer('azizbekcoderuz@gmail.com', 11111)
+
+transporter.sendMail(
+    {
+    from: "arzustartup@gmail.com",
+    to: "azizbekcoderuz@gmail.com", // to user
+    subject: "[Arzu] Verify code",
+    text: "",
+    html: `<b>Код подтверждения:</b> <code>1212123</code>. <b>Никому</b> не давайте код, даже если его требуют от имени <b>Arzu!</b>
+    <br><br>Этот код используется для входа в Ваш аккаунт в <b>Arzu</b>. Он никогда не нужен для чего-либо еще. 
+    <br><br>Если Вы не запрашивали код для входа, проигнорируйте это сообщение.`,
+    },
+    (err, info) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(info)
+    }
+); 
+
+
+
